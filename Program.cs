451f78f86f18756cddf3141261b518f4a74c511b
hodaadel77 <121638358+hodaadel77@users.noreply.github.com>
2022-12-29 22:52:28 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace discrete_task2
+namespace project._._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n1, n2, z = 0;
+            int n1, n2;
+            String flag = "prime";
             Console.WriteLine("enter n1");
             n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("enter n2");
             n2 = int.Parse(Console.ReadLine());
-            for (int i = n1; i <= n2; i++)
+            if (n1 >= 2)
             {
-                for (int x = 1; x < i; x++)
+                for (int i = n1; i <= n2; i++)
                 {
-                    if (i % x == 0)
+                    for (int x = 2; x < i; x++)
                     {
-                        z += x;
+                        if (i % x != 0)
+                        {
+                            flag = "prime";
+                        }
+                        else
+                        {
+                            flag = "not prime";
+                            break;
+                        }
+                    }
+
+                    if (flag == "prime")
+                    {
+                        Console.WriteLine(i);
                     }
                 }
-                if (z == i)
+            }
+            else
+            {
+                for (int i = 2; i <= n2; i++)
                 {
-                    Console.WriteLine(i);
+                    for (int x = 2; x < i; x++)
+                    {
+                        if (i % x != 0)
+                        {
+                            flag = "prime";
+                        }
+                        else
+                        {
+                            flag = "not prime";
+                            break;
+                        }
+                    }
+
+                    if (flag == "prime")
+                    {
+                        Console.WriteLine(i);
+                    }
                 }
-                z = 0;
             }
         }
     }
